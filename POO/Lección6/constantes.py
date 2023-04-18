e047e3e94264f37b85d9781b30e37ea90a7faba6
Jimena García <jgarcia.mca21@lania.edu.mx>
2023-04-18 09:26:21 -0600

    Constantes

---
+++ POO/Lección6/constantes.py
@@ -0,0 +1,7 @@
+# es una convensión en python. Hay una convención  con variables independientes y dentro de una clase
+# La CTE se escribe en mayus
+# Se escriban en un módulo por separado
+MI_CONSTANTE = 'Valor de mi constante'
+
+class Matemáticas:
+    PI = 3.1416 # variable de clase, no se crea un objeto para acceder ala clase
---
+++ POO/Lección6/test_constantes.py
@@ -0,0 +1,7 @@
+# from constantes import MI_CONSTANTE # si doy ctrl + espacio, automáticamente agrega la cte porque es la única en el módulo
+# from constantes import *
+from constantes import MI_CONSTANTE, Matemáticas as Mate # si se quiere configurar el nombre de la clase, se importan por separado
+
+print(MI_CONSTANTE)
+print(Mate.PI)
+# MI_CONSTANTE = 'hola ' # NO SE DEBE CAMBIAR UNA CONSTANTE AUNQUE SE PUEDA EN PYTHON
