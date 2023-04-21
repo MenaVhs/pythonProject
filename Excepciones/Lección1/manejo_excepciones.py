34e6bee5743bd9bd4e34ccd13c9e21044c9d0dc4
Jimena García <jgarcia.mca21@lania.edu.mx>
2023-04-20 21:42:47 -0600

    Exceptions

--- Excepciones/Lección1/manejo_excepciones.py
+++ Excepciones/Lección1/manejo_excepciones.py
@@ -1,7 +1,16 @@
+resultado = None
+a = '10'
+b = 0
 try:
-    10/0
-except Exception as e: # variable e de exceptio
-    print(f'Ocurrió un error: {e}')
+    resultado = a / b
+# except Exception as e: # variable e de exceptio
+#     print(f'Ocurrió un error: {e}')
 
-except ZeroDivisionError as e: # variable e de exceptio
-    print(f'Ocurrió un error: {e}')
+except ZeroDivisionError as zde: # variable e de exceptio
+    print(f'Ocurrió un error: {zde}, {type(zde)}')
+except TypeError as te:
+    print(f'Ocurrió un error: {te}', {type(te)})
+except Exception as e: # la clase más genérica debe ir al final
+    print(f'Ocurrió un error: {e}, {type(e)}')
+
+print('Resultado', resultado)
