c6f35b3296a9e5ba418ffedcb9d1bdc78a81ccb7
MenaVhs <jgg1b@live.com.mx>
2024-03-22 20:52:50 -0600

    continuacion de Django

--- Excepciones/Lección1/manejo_excepciones.py
+++ Excepciones/Lección1/manejo_excepciones.py
@@ -1,7 +1,15 @@
+from NumerosIdenticosException import NumerosIdenticosException
+
+
 resultado = None
-a = '10'
-b = 0
+
 try:
+    # validar dentro del try except
+    a = int(input('primer numero: '))
+    b = int(input('segundo numero: '))
+    if a == b:
+        raise NumerosIdenticosException('números idénticos')
+
     resultado = a / b
 # except Exception as e: # variable e de exceptio
 #     print(f'Ocurrió un error: {e}')
@@ -10,7 +18,21 @@ except ZeroDivisionError as zde: # variable e de exceptio
     print(f'Ocurrió un error: {zde}, {type(zde)}')
 except TypeError as te:
     print(f'Ocurrió un error: {te}', {type(te)})
+
+except ValueError as e:
+    print(f'Valueerror {e}')
 except Exception as e: # la clase más genérica debe ir al final
     print(f'Ocurrió un error: {e}, {type(e)}')
 
+else:
+    print('Todo en orden (:')
+
+finally:
+    # siempre se ejecuta
+    # aviso de un recurso o aviso al usuario de cómo terminó la ejecición2
+    print('ejecución del finally')
+
 print('Resultado', resultado)
+print("Continue...")
+
+
---
+++ Lamda/file.py
@@ -0,0 +1,5 @@
+mi_finción_lamda = lambda a, b: a + b
+
+resultado = mi_finción_lamda(2,3)
+
+print(resultado)
